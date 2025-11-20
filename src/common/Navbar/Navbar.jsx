36ed8dd37fd755d4d8f4384eca8628a51bcd16b3
Jang-YuJin <jangyj0912@naver.com>
2025-11-20 21:50:31 +0900

    feat: 로그인 시 유저아이콘 클릭하면 마이페이지, 로그아웃 나오도록 함

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -8,10 +8,15 @@ const Navbar = () => {
 
   const [login, setLogin] = useState(true);
   const [open, setOpen] = useState(false);
+  const [showProfileMenu, setShowProfileMenu] = useState(false);
   
   const goLogin = () => {
     navigate('/login');
   };
+
+  const goJoin = () => {
+    navigate('/join');
+  }
 
   return (
     <>
@@ -31,11 +36,21 @@ const Navbar = () => {
             <span></span>
           </div>
           { login
-          ? <div className={styles.user_icon}>U</div>
+          ? <div className={styles.user_icon} onClick={() => setShowProfileMenu(prev => !prev)}>U</div>
           : <div className={styles.login} onClick={goLogin}>LOGIN</div>
+          }
+          { !login &&
+          <div className={styles.login} onClick={goJoin}>JOIN</div>
           }
         </div>
       </div>
+
+      {login && showProfileMenu && (
+        <div className={styles.profile_menu}>
+          <div className={styles.menu_item}>마이페이지</div>
+          <div className={styles.menu_item}>로그아웃</div>
+        </div>
+      )}
 
       <MobileNavbar open={open} onClose={() => setOpen(false)} />
     </>
