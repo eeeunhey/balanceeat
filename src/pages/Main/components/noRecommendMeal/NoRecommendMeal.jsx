579639a55b74d7518733af21a006069c0817c68c
YunheeAhn <ayh95944674@gmail.com>
2025-11-23 04:24:49 +0900

    chore : 프리티어 적용으로 인한 수정(기능 변화는 없음)

--- src/pages/Main/components/noRecommendMeal/NoRecommendMeal.jsx
+++ src/pages/Main/components/noRecommendMeal/NoRecommendMeal.jsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react'
-import styles from './NoRecommendMeal.module.css'
-import { motion } from "motion/react"
-import { useMealStore } from '../../../../stores/useMealStore';
-import { getToday } from '../../../../utils/getToday'
+import React, { useEffect, useState } from "react";
+import styles from "./NoRecommendMeal.module.css";
+import { motion } from "motion/react";
+import { useMealStore } from "../../../../stores/useMealStore";
+import { getToday } from "../../../../utils/getToday";
 
 const NoRecommendMeal = () => {
-  const {getMealsByDate} = useMealStore();
+  const { getMealsByDate } = useMealStore();
   const [meals, setMeals] = useState(getMealsByDate(getToday()));
   const [noTodayMeal, setNoTodayMeal] = useState(true);
 
   const fadeUp = {
-    hidden: {opacity: 0, y: 60},
-    show: {opacity: 1, y: 0, transition: {duration: 0.6}}
+    hidden: { opacity: 0, y: 60 },
+    show: { opacity: 1, y: 0, transition: { duration: 0.6 } },
   };
 
   useEffect(() => {
     console.log(meals);
-    if(meals.breakfast.length === 0){
+    if (meals.breakfast.length === 0) {
       setNoTodayMeal(true);
-    } else{
+    } else {
       setNoTodayMeal(false);
     }
   }, [meals]);
@@ -28,8 +28,10 @@ const NoRecommendMeal = () => {
       <h2>부족 영양소 기반 메뉴 추천</h2>
       <div className={styles.today_rec_box}>
         <div className={styles.recommend_list}>
-          <div className={styles.small_content}>{noTodayMeal ? '오늘의 식단을 입력하면' : '오늘의 요약을 보러가시면'}</div>
-          <div className={styles.small_content}>부족한 영양소를 기반으로</div>
+          <div className={styles.small_content}>
+            {noTodayMeal ? "오늘의 식단을 입력하면" : "오늘의 요약을 보러가시면"} <br />
+            부족한 영양소를 기반으로
+          </div>
           <div className={styles.content}>메뉴를 추천하고 있어요! 🍱</div>
         </div>
       </div>
